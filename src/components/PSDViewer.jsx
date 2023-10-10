4cc71134638480364424f10b4afbc525ff465e64
thapsamit <thapliyalamit2001@gmail.com>
2023-10-10 18:15:18 +0530

    added docs

--- src/components/PSDViewer.jsx
+++ src/components/PSDViewer.jsx
@@ -22,7 +22,7 @@ export default function PSDViewer() {
         // The document to open.
 
         disableWebAssemblyStreaming: true,
-        document: require("../assets/resume.doc"),
+        document: require("../assets/sample2.pdf"),
 
         // document:
         //   "https://insp-development-bucket.s3.ap-south-1.amazonaws.com/files/roomId_F5vOsurp3D/resume.docx?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAZHE7QK27AWMDOJS6%2F20231005%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Date=20231005T035005Z&X-Amz-Expires=3600&X-Amz-Signature=88a27c2d4e34ee170dc6aa0231696e025dc6dbae581cc76dc0e138d3fe06bf96&X-Amz-SignedHeaders=host",
